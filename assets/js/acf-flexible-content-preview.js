6673b97aae621451d70064ef8db71f1661d3462c
Jameel Moses <jameel@interactivestrategies.com>
2019-12-02 08:40:43 -0500

    Resolving #3 Preview Images Missing

--- assets/js/acf-flexible-content-preview.js
+++ assets/js/acf-flexible-content-preview.js
@@ -1,8 +1,8 @@
 // Delete the flexible content preview popup if only one layout
-$(document).ready(function($) {
+jQuery(document).ready(function(jQuery) {
   var flexible_content_open = acf.getField('acf-field-flexible-content');
   flexible_content_open._open = function(e) {
-    var $popup = $(this.$el.children('.tmpl-popup').html());
+    var $popup = jQuery(this.$el.children('.tmpl-popup').html());
     if ($popup.find('a').length == 1) {
       // Only one layout
       flexible_content_open.add($popup.find('a').attr('data-layout'));
@@ -13,11 +13,11 @@ $(document).ready(function($) {
 });
 
 // Transform a link into a div for styling purpose
-$('body').on('click', 'a[data-name="add-layout"]', function() {
+jQuery('body').on('click', 'a[data-name="add-layout"]', function() {
   setTimeout(function() {
-    $('.acf-fc-popup a').each(function() {
-      var html = '<div class="acf-fc-popup-label">' + $(this).text() + '</div><div class="acf-fc-popup-image"></div>';
-      $(this).html(html);
+    jQuery('.acf-fc-popup a').each(function() {
+      var html = '<div class="acf-fc-popup-label">' + jQuery(this).text() + '</div><div class="acf-fc-popup-image"></div>';
+      jQuery(this).html(html);
     });
   }, 0);
 });
